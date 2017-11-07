5960d1daafe4a0306d64b81e43cf8ca387caff7b
Grzegorz <mogadish@gmail.com>
2017-11-07 10:25:01 +0100

    package struct

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+ name='test_py_module',
+ version='0.1',
+ description='test module',
+ url='https://github.com/grzegorzme/test_py_module',
+ author='Grzegorz Melniczak',
+ author_email='',
+ license='MIT',
+ packages=['test_py_module'],
+ zip_safe=False
+)
+
+      
